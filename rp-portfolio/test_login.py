ae89a6e3766c7076b2372d20416d66a70059f597
Arm57ar <34701555+Arm57ar@users.noreply.github.com>
2019-11-23 11:19:29 +0800

    Add files via upload

---
+++ rp-portfolio/test_login.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pytest
+import time
+
+driver = webdriver.Chrome()
+driver.get("http://localhost:8000/admin/login/?next=/admin/")
+elem = driver.find_element_by_name("username")
+elem.send_keys("S10182342")
+
+elem = driver.find_element_by_name("password")
+elem.send_keys("password")
+elem.send_keys(Keys.RETURN) #press enter key
+time.sleep(2)
+
+assert driver.current_url == 'http://localhost:8000/admin/login/?next=/admin/'
+
+
+driver.close()
+
