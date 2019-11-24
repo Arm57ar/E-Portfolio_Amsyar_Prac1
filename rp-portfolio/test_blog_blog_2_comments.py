9e491564aac8daf5ac15ada33a2f8160450d86c5
AsrMzr <amsyar96@hotmail.com>
2019-11-24 16:22:32 +0800

    Final Blog1 and 2 comments sections

--- rp-portfolio/test_blog_blog_2_comments.py
+++ rp-portfolio/test_blog_blog_2_comments.py
@@ -5,21 +5,20 @@ import time
 
 # It should go to the page and able to key in the following Name and comments .
 driver = webdriver.Chrome()
-driver.get("http://localhost:8000/blog/1/")
+driver.get("http://localhost:8000/blog/2/")
 elem = driver.find_element_by_name("author")
-elem.send_keys("AmsyarMazri")
+elem.send_keys("Mr John")
 
 elem = driver.find_element_by_name("body")
-elem.send_keys("Hello , is it good to know you are helping this organization.")
+elem.send_keys("Hello , can you share your experience on how you manage to overcome this .Thanks")
 elem.send_keys(Keys.RETURN) #press enter key
 
 # once it click submit, it will go to comments sections.
 #Pass value. 
 elem = driver.find_element_by_name("submit").click()
-
 #time.sleep(2)
 
-assert driver.current_url == 'http://localhost:8000/blog/1/'
+assert driver.current_url == 'http://localhost:8000/blog/2/'
 
 
 
